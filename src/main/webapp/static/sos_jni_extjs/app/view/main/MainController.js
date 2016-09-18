0fc3f1d57aadfc6f94c4406cf984d1ead7508c04
turningOwei <175408322@qq.com>
2016-09-18 12:35:30 +0800

    controller+mainmodel

--- src/main/webapp/static/sos_jni_extjs/app/view/main/MainController.js
+++ src/main/webapp/static/sos_jni_extjs/app/view/main/MainController.js
@@ -4,7 +4,7 @@
  *
  * TODO - Replace this content of this view to suite the needs of your application.
  */
-Ext.define('Sos.jni.controller.MainController', {
+Ext.define('Sos.jni.view.main.MainController', {
     extend: 'Ext.app.ViewController',
 
     alias: 'controller.main',
